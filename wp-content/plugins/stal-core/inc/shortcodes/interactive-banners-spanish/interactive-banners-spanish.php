50608b2ff6a4db998571d5418d491e565b31eb93
Federico Vergara <hu.ce.ro@gmail.com>
2022-03-26 21:19:39 -0300

    Interqactive columns spanish

--- wp-content/plugins/stal-core/inc/shortcodes/interactive-banners-spanish/interactive-banners-spanish.php
+++ wp-content/plugins/stal-core/inc/shortcodes/interactive-banners-spanish/interactive-banners-spanish.php
@@ -9,7 +9,7 @@ if ( ! function_exists( 'stal_core_add_interactive_banners_shortcode' ) ) {
 	 * @return array
 	 */
 	function stal_core_add_interactive_banners_shortcode( $shortcodes ) {
-		$shortcodes[] = 'StalCoreInteractiveBannersShortcode';
+		$shortcodes[] = 'StalCoreInteractiveBannersSpanishShortcode';
 		
 		return $shortcodes;
 	}
@@ -18,7 +18,7 @@ if ( ! function_exists( 'stal_core_add_interactive_banners_shortcode' ) ) {
 }
 
 if ( class_exists( 'StalCoreShortcode' ) ) {
-	class StalCoreInteractiveBannersShortcode extends StalCoreShortcode {
+	class StalCoreInteractiveBannersSpanishShortcode extends StalCoreShortcode {
 		
 		public function map_shortcode() {
 			$this->set_shortcode_path( STAL_CORE_SHORTCODES_URL_PATH . '/interactive-banners-spanish' );
